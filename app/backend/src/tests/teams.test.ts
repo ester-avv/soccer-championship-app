9fe9ceec011913608160110eede3a268fc9c9a8b
Ester Viana Verza <ester.av@hotmail.com>
2023-06-02 11:29:20 -0300

    fix testes /teams

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,8 +1,8 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
-/* import chaiHttp = require('chai-http'); */
-import chaiHttp from 'chai-http';
+import chaiHttp = require('chai-http'); 
+/* import chaiHttp from 'chai-http'; */
 
 import { app } from '../app';
 
@@ -12,32 +12,33 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-import Teams from '../database/models/teams.model';
-import TeamsService from '../services/teams.service';
-import TeamsController from '../controllers/teams.controller';
+import TeamsModel from '../database/models/teams.model';
+
 import { teamsMock, palmeiras } from './mocks/teams.mock';
+import TeamsService from '../services/teams.service';
+import Team from '../interfaces/team';
 /* import Team from '../interfaces/team'; */
 
 describe('Testes da rota /teams', () => {
-    it('01- Verifica o endpoint get', async () => {
+    it('Verifica o endpoint get para pegar todos os times', async () => {
   
-     sinon.stub(Teams, 'findAll')
-       .resolves(teamsMock as Teams[]);
+     sinon.stub(TeamsModel, 'findAll')
+       .resolves(teamsMock as TeamsModel[]);
   
       const response = await chai.request(app).get('/teams');
   
       expect(response.body).to.be.equal(teamsMock)
-      expect(response.status).to.be.equal(200);
+      expect(response).to.have.status(200);
     });
   
-    it('02- Verifica se é possível buscar por id', async () => {
+    it('Verifica se é possível buscar por id', async () => {
   
-      sinon.stub(Teams, 'findByPk')
-       .resolves(palmeiras as Teams);
+      sinon.stub(TeamsModel, 'findByPk')
+       .resolves(palmeiras as TeamsModel);
   
       const response = await chai.request(app).get('/teams/12');
   
-      expect(response.status).to.be.equal(200);
+      expect(response).to.have.status(200);
       expect(response.body).to.be.equal(palmeiras)
     });
   });
